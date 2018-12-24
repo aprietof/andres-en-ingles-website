609b06a0cbc111591f391645d94a173b19925caf
Adrian Prieto <adrianprieto00@gmail.com>
2018-12-24 09:38:04 -0500

    Update year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { format } from 'date-fns';
 import NewsletterSubscribeForm from './NewsletterSubscribeForm';
 
 export default function Footer() {
@@ -20,26 +21,38 @@ export default function Footer() {
             andrespfranklin@gmail.com
           </a>
         </div>
-        <NewsletterSubscribeForm/>
+        <NewsletterSubscribeForm />
       </section>
       <div className="dt dt--fixed w-100">
         <div className="dn dtc-ns v-mid">
-          <p className="f7 black-70 dib pr3 mb3">Copyright © absolutandres 2018</p>
+          <p className="f7 black-70 dib pr3 mb3">Copyright © absolutandres {format(new Date(), 'YYYY')}</p>
         </div>
         <div className="db dtc-ns black-70 tc tr-ns v-mid">
-          <a href="https://www.facebook.com/absolutandres" className="link dim dib mr3 black-70" title="Andres en ingles on Facebook">
+          <a
+            href="https://www.facebook.com/absolutandres"
+            className="link dim dib mr3 black-70"
+            title="Andres en ingles on Facebook"
+          >
             <svg className="db w2 h2" data-icon="facebook" viewBox="0 0 32 32" fill="currentColor">
               <title>facebook icon</title>
               <path d="M8 12 L13 12 L13 8 C13 2 17 1 24 2 L24 7 C20 7 19 7 19 10 L19 12 L24 12 L23 18 L19 18 L19 30 L13 30 L13 18 L8 18 z" />
             </svg>
           </a>
-          <a href="https://twitter.com/absolutandres" className="link dim dib mr3 black-70" title="Andres en ingles on Twitter">
+          <a
+            href="https://twitter.com/absolutandres"
+            className="link dim dib mr3 black-70"
+            title="Andres en ingles on Twitter"
+          >
             <svg className="db w2 h2" data-icon="twitter" viewBox="0 0 32 32" fill="currentColor">
               <title>twitter icon</title>
               <path d="M2 4 C6 8 10 12 15 11 A6 6 0 0 1 22 4 A6 6 0 0 1 26 6 A8 8 0 0 0 31 4 A8 8 0 0 1 28 8 A8 8 0 0 0 32 7 A8 8 0 0 1 28 11 A18 18 0 0 1 10 30 A18 18 0 0 1 0 27 A12 12 0 0 0 8 24 A8 8 0 0 1 3 20 A8 8 0 0 0 6 19.5 A8 8 0 0 1 0 12 A8 8 0 0 0 3 13 A8 8 0 0 1 2 4" />
             </svg>
           </a>
-          <a href="https://www.youtube.com/user/soyimaginario" className="link dim dib mr3 black-70" title="Andres en ingles on youtube">
+          <a
+            href="https://www.youtube.com/user/soyimaginario"
+            className="link dim dib mr3 black-70"
+            title="Andres en ingles on youtube"
+          >
             <svg
               className="dib w2 h2"
               fill="currentColor"
@@ -73,7 +86,6 @@ export default function Footer() {
               </g>
             </svg>
           </a> */}
-          
         </div>
       </div>
       <div className="db dn-ns">
